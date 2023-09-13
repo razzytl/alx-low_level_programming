e93cacaa2f65b2bce124ce701429545f11ca8f93
razzytl <youssefbenyaich1@gmail.com>
2023-09-12 17:05:28 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int n);
+
+#endif /* MAIN_H */
